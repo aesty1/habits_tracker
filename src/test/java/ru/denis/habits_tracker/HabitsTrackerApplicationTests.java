15afb94e5baeb2f3887f580dca423acbd60b5d76
aesty1 <kadirovdenis123@gmail.com>
2026-06-22 13:35:00 +0500

    first commit

---
+++ src/test/java/ru/denis/habits_tracker/HabitsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.denis.habits_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
